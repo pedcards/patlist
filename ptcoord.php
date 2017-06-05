1d819d6943a88af6f2392d47edbe04b1732bd05d
Terry Chun <docterry@hotmail.com>
2017-06-04 21:46:19 -0700

    Improve layout of Status flags

--- ptcoord.php
+++ ptcoord.php
@@ -227,9 +227,6 @@ function dialogConfirm() {
         <label for="cbox-1b">Pillow given</label>
         <input name="statusTour" id="cbox-1c" type="checkbox" <?php if ($statusTour) { echo 'checked="checked"'; } ?> onChange="submit();">
         <label for="cbox-1c">Tour given</label>
-        <!--<input data-icon="camera" data-iconpos="notext" data-corners="false" value="Icon only" type="submit" >-->
-    </fieldset>
-    <fieldset data-role="controlgroup" data-type="horizontal" data-mini="true" class="ui-field-contain">
         <input name="statusMFM" id="cbox-2a" type="checkbox" <?php if ($statusMFM) { echo 'checked="checked"'; } ?> onChange="submit();">
         <label for="cbox-2a">MFM notified</label>
         <!--<input data-icon="camera" data-iconpos="notext" data-corners="false" value="Icon only" type="submit" >-->
